b48b91b5e425ccd297f4a71be1019887da508910
Derek Kaser <derek.kaser@gmail.com>
2023-07-04 22:29:50 -0400

    fix: switch to using command line ZIP

--- src/usr/local/emhttp/plugins/plugin-diagnostics/download.php
+++ src/usr/local/emhttp/plugins/plugin-diagnostics/download.php
@@ -15,51 +15,47 @@ $timestamp = date("Ymd-His");
 $filename  = gethostname() . "-{$_GET['plugin']}-diag-{$timestamp}";
 
 $diagnosticsFile       = "/tmp/{$filename}.zip";
-$systemDiagnosticsFile = "/tmp/system-diagnostics-{$timestamp}.zip";
+$diagnosticsFolder     = "/tmp/{$filename}";
+$systemDiagnosticsFile = "{$diagnosticsFolder}/system-diagnostics.zip";
 
-// Create the diagnostic archive
-$zip = new ZipArchive();
-if ($zip->open($diagnosticsFile, ZipArchive::CREATE) !== true) {
-    exit("cannot create diagnostic file {$diagnosticsFile}\n");
-}
+mkdir($diagnosticsFolder, 0755);
 
 // Add system diagnostics
 if (array_key_exists("system_diagnostics", $config) ? $config["system_diagnostics"] : false) {
     exec("diagnostics '{$systemDiagnosticsFile}'");
-    if (file_exists($systemDiagnosticsFile)) {
-        $zip->addFile($systemDiagnosticsFile, "{$filename}/system-diagnostics.zip");
-    }
 }
 
 // Run commands and save output
 if (array_key_exists("commands", $config)) {
+    mkdir("{$diagnosticsFolder}/commands", 0755);
     foreach ($config["commands"] as $command) {
-        $zip->addFromString("{$filename}/commands/{$command['file']}", shell_exec($command["command"]));
+        file_put_contents("{$diagnosticsFolder}/commands/{$command['file']}", shell_exec($command["command"]));
     }
 }
 
 // Collect files
 if (array_key_exists("files", $config)) {
+    mkdir("{$diagnosticsFolder}/files", 0755);
     foreach ($config["files"] as $file) {
         if (file_exists($file)) {
-            $zip->addFile($file, "{$filename}/files/" . ltrim($file, '/'));
+            $destFile = "{$diagnosticsFolder}/files{$file}";
+            mkdir(dirname($destFile), 0755, true);
+            copy($file, $destFile);
         }
     }
 }
 
-// Close the archive
-$zip->close();
-
-// Send the file
-header('Content-type: application/zip');
-header('Content-Disposition: attachment; filename="' . $filename . '.zip"');
-header("Pragma: no-cache");
-header("Expires: 0");
-readfile($diagnosticsFile);
+exec("cd /tmp && zip -qmr " . escapeshellarg($diagnosticsFile) . " " . escapeshellarg("{$filename}/"));
 
 if (file_exists($diagnosticsFile)) {
+    // Send the file
+    header('Content-type: application/zip');
+    header('Content-Disposition: attachment; filename="' . $filename . '.zip"');
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    readfile($diagnosticsFile);
+
     unlink($diagnosticsFile);
-}
-if (file_exists($systemDiagnosticsFile)) {
-    unlink($systemDiagnosticsFile);
+} else {
+    echo "Could not generate diagnostic package.";
 }
